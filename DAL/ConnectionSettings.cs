c4a083c693871423f6421b00f9ea1251e8e5646c
Hasan ÖZEN <33752633+hasanozen@users.noreply.github.com>
2021-01-09 19:55:05 +0300

    Update ConnectionSettings.cs
    Solved DB path issue

--- DAL/ConnectionSettings.cs
+++ DAL/ConnectionSettings.cs
@@ -1,5 +1,4 @@
 ﻿public static class ConnectionSettings
 {
     public const string CONNECTION_STRING = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=LMSDB.accdb";
-    //C:\Users\MAERIS\source\repos\SimpleLibraryManagementSystem\SimpleLibraryManagementSystem\bin\Debug\
 }
